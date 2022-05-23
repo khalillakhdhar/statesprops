3ae1837da96d82cfb7c66b041bca7899f4bbe3d0
khalil <khalillakhdharatc@gmail.com>
2022-05-23 13:49:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import React, { Component } from 'react';
-class App extends React.Component {
-constructor() {
-super();
-//let etat=true; (instable)=> perte de valeur lors de l'actualisation
-this.state = { displayBio: false }; // stateful component qui preserve l'etat si le component est chargé
-console.log('Component this', this);
-this.toggleBio = this.toggleBio.bind(this); // actualisation par rapport à l'éta
-// bind pour force la relecture de nouvelle valeur
-
+class App extends Component {
+constructor(props){
+super(props);
+this.state = {
+data: 'www.reactjs.org'
 }
-toggleBio(){
-  this.setState({displayBio: !this.state.displayBio});
-  }
-  // Il est recommander de placer les méthodes personalisées aprés le constructeur et avant le render
-
-render() {
-const bio = this.state.displayBio ? (
-<div>
-<p><h3>React is one of the best js libraries.</h3></p>
-</div>
-) : null;
+this.handleEvent = this.handleEvent.bind(this);
+}
+handleEvent(){
+console.log(this.props);
+} render() {
 return (
-<div>
-<h1>Welcome to React!!</h1>
-{
-this.state.displayBio ? (
-<div>
-<p><h4>{ this.props.librairie } is wonderfull.</h4></p>
-<button onClick={this.toggleBio}> Show Less </button>
+<div className="App">
+<h2>React Constructor Example</h2>
+<input type ="text" value={this.state.data} />
+<button onClick={this.handleEvent}>Please Click</button>
 </div>
-) : (
-<div>
-<button onClick={this.toggleBio}> Read More </button>
-</div>
-)
-}
-</div>
-) } }
-export default App;
+); } } export default App;
