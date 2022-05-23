c3aed827df8b1019681857d1351f6cb52cccdb6c
khalil <khalillakhdharatc@gmail.com>
2022-05-23 14:20:01 +0100

    cc

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ class App extends Component {
 constructor(props){
 super(props);
 this.state = {
-data: 'www.reactjs.org'
+value: 'www.reactjs.org'
 }
 this.handleEvent = this.handleEvent.bind(this);
+this.handleInputChange = this.handleInputChange.bind(this);
+}
+setData(event)
+{
+  this.setState({value:event.target.value});
 }
 handleEvent(){
-console.log(this.props);
-} render() {
+console.log("props",this.props);
+console.log("states",this.state);
+} 
+handleInputChange(event) {
+  const target = event.target;
+  const value =  target.value;
+  const name = target.name;
+  this.setState({
+  [name]: value
+  });
+  }
+render() {
 return (
 <div className="App">
 <h2>React Constructor Example</h2>
-<input type ="text" value={this.state.data} />
+<input type ="text" value={this.state.value} name="value"  onChange={this.handleInputChange} />
 <button onClick={this.handleEvent}>Please Click</button>
 </div>
 ); } } export default App;
